6ad7b6f62dd52f923e0c404cf8cf0fcf238d6494
Lauren Main <lauren.main28@gmail.com>
2021-12-18 15:30:41 -0800

    add yes/no question

---
+++ javascript/app.js
@@ -0,0 +1,17 @@
+let holiday = prompt "After reading this page, do you like Presidents day?"; 
+let message ="";
+
+if (answer=Yes) {
+    message= "Great news!!"
+}
+
+else if (answer=no){
+    message= "Ouch! You need more holidays!"
+}
+
+else (answer=''){
+    message= "Better luck next time!"
+}
+
+console:log holiday
+
