39b90101c7db72bc79db24e716e5c7cf76019432
Lauren Main <lauren.main28@gmail.com>
2021-12-18 20:38:17 -0800

    add teammate link, add correct js link

--- javascript/app.js
+++ javascript/app.js
@@ -1,15 +1,15 @@
-let holiday = prompt ("After reading this page, do you like Presidents day?"); 
+let holiday = prompt ("Have you ever celebrated Presidents Day?!"); 
 let message ="";
 
 if (answer=Yes) {
     message= "Great news!!"
 }
 
-else if (answer=no){
+else if (answer=no) {
     message= "Ouch! You need more holidays!"
 }
 
-else if (answer=""){
+else if (answer="") {
     message= "Better luck next time!"
 }
 
